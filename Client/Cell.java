84a5293ffef78d8142c6e7e11a3c5f9a2ee01c5f
Terry Carson <terryrcarson@gmail.com>
2015-03-18 15:12:56 -0400

    Fix directory structure

---
+++ Client/Cell.java
@@ -0,0 +1,52 @@
+/**
+ * @(#)Cell.java
+ *
+ *
+ * @author 
+ * @version 1.00 2015/3/6
+ */
+
+import java.io.Serializable;
+
+public class Cell implements Serializable {
+
+	//Types: 0 = Empty, 1 = Red, 2 = Black
+	private int x, y, type;
+	private transient final int RED = 1, BLACK = 2;
+	
+	public Cell() {}
+	
+    public Cell(int x, int y) { 
+    	this.x = x;
+    	this.y = y;
+    }
+    
+    public void setType(int type) {
+    	this.type = type;
+    }
+    
+    public int getType(){
+    	return type;
+    }
+    
+    public int getX() { return x; }
+
+    public int getY() { return y; }
+
+    public void setX(int x) { this.x = x; }
+
+    public void setY(int y) { this.y = y; }
+    
+    public void incX() { x++; }
+    
+    public void decX() { x--; }
+    
+    @Override
+    public boolean equals(Object o) {
+    	if (o instanceof Cell) {
+    		Cell other = (Cell) o;
+    		return (x == other.x && y == other.y);
+    	}
+    	return false;
+    }
+}
